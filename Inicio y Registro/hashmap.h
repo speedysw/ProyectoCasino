1862ab44f91253d675b45c6aa1258486a101823d
speedysw <cesar30paolo@gmail.com>
2021-06-17 22:50:16 -0400

    Primera Subida

---
+++ Inicio y Registro/hashmap.h
@@ -0,0 +1,21 @@
+#ifndef HashMap_h
+#define HashMap_h
+
+typedef struct HashMap HashMap;
+
+HashMap *createMap(long capacity);
+
+void insertMap(HashMap *table, char *key, void *value);
+
+void eraseMap(HashMap *table, char *key);
+
+void *searchMap(HashMap *table, char *key);
+
+void *firstMap(HashMap *table);
+
+void *nextMap(HashMap *table);
+
+
+void enlarge(HashMap *map);
+
+#endif /* HashMap_h */
